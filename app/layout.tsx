875e3602b91589f93a1e524b8fa232b94864238f
vitormalencar <vitor.maia.alencar@gmail.com>
2024-10-24 13:30:48 +0200

    Refactor layout.tsx to include background color in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,7 +58,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-200`}
       >
         {children}
       </body>
